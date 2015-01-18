ca8922feec4a99ff1f7db584d97bb6e93c1a1bce
Ivan Kyosev <2092065K@student.gla.ac.uk>
2015-01-18 01:19:38 +0000

    Chapter 6 - completed

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+admin.AdminSite.site_header = "Rango's sheriff office"
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'views', 'likes']
+    list_display = ('name', 'views', 'likes')
+
+admin.site.register(Category, CategoryAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ['title', 'category', 'url', 'views']
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
