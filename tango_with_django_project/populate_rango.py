0ea2dfb52d47cd59b75a36921771f4395892a7fb
Ivan Kyosev <2092065K@student.gla.ac.uk>
2015-01-18 20:11:27 +0000

    Chapter 7 - completed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,49 +12,59 @@ def populate():
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=7)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=4)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=4)
 
     django_cat = add_cat(name="Django", views=64, likes=32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=11)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=5)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=20)
 
     frame_cat = add_cat(name="Other Frameworks", views=32, likes=16)
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=10)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=14)
         
     my_cat = add_cat(name = "2092065k")
      
     add_page(cat=my_cat,
        title="Git Hub",
-       url="https://github.com/2092065k")
+       url="https://github.com/2092065k",
+       views=2)
         
     add_page(cat=my_cat,
        title="Python anywhere",
-       url="https://www.pythonanywhere.com/user/2092065k")
+       url="https://www.pythonanywhere.com/user/2092065k",
+       views=1)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
