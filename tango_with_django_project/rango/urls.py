5dc56922d9cdffce2d55416fc2cdc7f3a8e7d340
Ivan Kyosev <2092065K@student.gla.ac.uk>
2015-03-08 14:39:11 +0000

    Chapter 19 with the exercise but there is still a bug

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
         url(r'^edit_profile/$', views.edit_profile, name = 'edit_profile'),
         url(r'^users/$', views.users, name = 'users'),
         url(r'^like_category/$', views.like_category, name='like_category'),
-        url(r'^suggest_category/$', views.suggest_category, name='suggest_category'))
+        url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
+        url(r'^auto_add_page/$', views.auto_add_page, name='auto_add_page'))
